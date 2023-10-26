eca7f5e6d19af16dbc7174e7c7eb286bfec36bd5
ThaerHussein <tselawe706@gmail.com>
2023-10-26 18:32:18 +0300

    class 30

---
+++ class-30/Demo/HashMap/app/src/main/java/hashmap/HashMapPair.java
@@ -0,0 +1,11 @@
+package hashmap;
+
+import java.util.AbstractMap;
+
+public class HashMapPair<K, V> extends AbstractMap.SimpleEntry<K, V>
+{
+    public HashMapPair(K key, V value)
+    {
+        super(key, value);
+    }
+}
