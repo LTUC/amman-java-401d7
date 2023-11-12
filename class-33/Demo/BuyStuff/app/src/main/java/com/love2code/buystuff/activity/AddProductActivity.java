e3ab68aa1fceb8bb250bffae352d6bb48e77f2af
ThaerHussein <tselawe706@gmail.com>
2023-11-12 14:29:42 +0300

    class 33

---
+++ class-33/Demo/BuyStuff/app/src/main/java/com/love2code/buystuff/activity/AddProductActivity.java
@@ -0,0 +1,119 @@
+package com.love2code.buystuff.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.core.model.temporal.Temporal;
+import com.amplifyframework.datastore.generated.model.Contact;
+import com.amplifyframework.datastore.generated.model.Product;
+import com.amplifyframework.datastore.generated.model.ProductCategoryEnum;
+import com.google.android.material.snackbar.Snackbar;
+import com.love2code.buystuff.R;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class AddProductActivity extends AppCompatActivity {
+
+    public static final String TAG = "AddProductActivity";
+
+    Spinner contactSpinner= null;
+
+    Spinner productCategorySpinner= null;
+    CompletableFuture<List<Contact>> contactFuture = new CompletableFuture<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+
+        contactFuture = new CompletableFuture<>();
+        setUpSpinners();
+        setUpSaveButton();
+    }
+
+    private void setUpSpinners(){
+        contactSpinner = (Spinner) findViewById(R.id.addProductContactSpinner);
+
+        Amplify.API.query(
+                ModelQuery.list(Contact.class),
+                success ->
+                {
+                    Log.i(TAG, "Read Contact Successfully");
+                    ArrayList<String> contactNames = new ArrayList<>();
+                    ArrayList<Contact> contacts = new ArrayList<>();
+                    for(Contact contact: success.getData()){
+                        contacts.add(contact);
+                        contactNames.add(contact.getFullName());
+                    }
+                    contactFuture.complete(contacts);
+
+                    runOnUiThread(() ->
+                    {
+                        contactSpinner.setAdapter(new ArrayAdapter<>(
+                                this,
+                                (android.R.layout.simple_spinner_item),
+                                contactNames
+                        ));
+                    });
+                },
+                failure-> {
+                    contactFuture.complete(null);
+                    Log.i(TAG, "Did not read contacts successfully");
+                }
+        );
+        productCategorySpinner = (Spinner) findViewById(R.id.addProductProductCategorySpinner);
+        productCategorySpinner.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                ProductCategoryEnum.values()
+        ));
+    }
+
+    private void setUpSaveButton(){
+        Button saveButton = (Button) findViewById(R.id.saveProductButton);
+        saveButton.setOnClickListener(v -> {
+
+            String name = ((EditText)findViewById(R.id.addProductProductNameEditTExt)).getText().toString();
+            String description = ((EditText)findViewById(R.id.addProductProductDescriptionEditText)).getText().toString();
+            String currentDateString = com.amazonaws.util.DateUtils.formatISO8601Date(new Date());
+            String selectedContactString = contactSpinner.getSelectedItem().toString();
+
+            List<Contact> contacts=null;
+            try {
+                contacts=contactFuture.get();
+            }catch (InterruptedException ie){
+                Log.e(TAG, " InterruptedException while getting contacts");
+            }catch (ExecutionException ee){
+                Log.e(TAG," ExecutionException while getting contacts");
+            }
+
+            Contact selectedContact = contacts.stream().filter(c -> c.getFullName().equals(selectedContactString)).findAny().orElseThrow(RuntimeException::new);
+
+            Product newProduct = Product.builder()
+                    .name(name)
+                    .description(description)
+                    .dateCreated(new Temporal.DateTime(currentDateString))
+                    .productCategory((ProductCategoryEnum) productCategorySpinner.getSelectedItem())
+                    .contactPerson(selectedContact).build();
+
+            Amplify.API.mutate(
+                    ModelMutation.create(newProduct),
+                    successResponse -> Log.i(TAG, "AddProductActivity.onCreate(): made a product successfully"),//success response
+                    failureResponse -> Log.e(TAG, "AddProductActivity.onCreate(): failed with this response" + failureResponse)// in case we have a failed response
+            );
+            Snackbar.make(findViewById(R.id.addProductActivity), "Product saved!", Snackbar.LENGTH_SHORT).show();
+        });
+    }
+}
---
+++ class-33/Demo/BuyStuff/app/src/main/java/com/love2code/buystuff/activity/ProductListActivity.java
@@ -0,0 +1,155 @@
+package com.love2code.buystuff.activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Product;
+import com.love2code.buystuff.R;
+import com.love2code.buystuff.activity.adapter.ProductListRecyclerVIewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductListActivity extends AppCompatActivity {
+
+    public static final String PRODUCT_NAME_EXTRA_TAG = "productName";
+    public final String TAG = "productActivity";
+    SharedPreferences preferences;
+
+    List<Product> products = null;
+
+    ProductListRecyclerVIewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+
+//        Contact contact1=
+//                Contact.builder()
+//                        .email("thaer27@gmail.com")
+//                        .fullName("Thaer Selawe")
+//                        .build();
+//        Contact contact2=
+//                Contact.builder()
+//                        .email("Jon@gmail.com")
+//                        .fullName("John marten")
+//                        .build();
+//        Contact contact3=
+//                Contact.builder()
+//                        .email("Sara@gmail.com")
+//                        .fullName("Sara anyname")
+//                        .build();
+//       Amplify.API.mutate(
+//               ModelMutation.create(contact1),
+//               successResponse -> Log.i(TAG, "ProductListActivity.onCreate(): made a contact successfully"),
+//               failureResponse -> Log.i(TAG, "ProductListActivity.onCreate(): contact failed with this response: "+failureResponse)
+//       );
+//        Amplify.API.mutate(
+//                ModelMutation.create(contact2),
+//                successResponse -> Log.i(TAG, "ProductListActivity.onCreate(): made a contact successfully"),
+//                failureResponse -> Log.i(TAG, "ProductListActivity.onCreate(): contact failed with this response: "+failureResponse)
+//        );
+//        Amplify.API.mutate(
+//                ModelMutation.create(contact3),
+//                successResponse -> Log.i(TAG, "ProductListActivity.onCreate(): made a contact successfully"),
+//                failureResponse -> Log.i(TAG, "ProductListActivity.onCreate(): contact failed with this response: "+failureResponse)
+//        );
+        init();
+        setUpOrderButton();
+        setUpSettingsImageView();
+        setUpProductListRecyclerView();
+        setUpAddProductButton();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String userNickname = preferences.getString(UserProfileActivity.USER_NICKNAME_TAG, "No nickname");
+
+        ((TextView) findViewById(R.id.userNicknameTextView)).setText(getString(R.string.nickname_with_input, userNickname));
+        Amplify.API.query(
+                ModelQuery.list(Product.class),
+                success ->
+                {
+                    Log.i(TAG, "Read Product successfully");
+                    //products = new ArrayList<>();
+                    products.clear();
+                    for (Product databaseProduct : success.getData()) {
+                        String contactName= "John marten";
+                        if(databaseProduct.getContactPerson().getFullName().equals(contactName)) {
+                            products.add(databaseProduct);
+                        }
+                    }
+                    //adapter.notifyDataSetChanged();
+                    runOnUiThread(() -> {
+                        adapter.notifyDataSetChanged();
+                    });
+                },
+                failure -> Log.i(TAG, "Did not read products successfully")
+        );
+    }
+
+    private void setUpOrderButton() {
+
+        Button orderButton = findViewById(R.id.productListOrderButton);
+
+        orderButton.setOnClickListener(V -> {
+
+            String productName = ((EditText) findViewById(R.id.productNameEditText)).getText().toString();
+
+            Intent goToOrderFromIntent = new Intent(ProductListActivity.this, OrderFormActivity.class);
+            goToOrderFromIntent.putExtra(PRODUCT_NAME_EXTRA_TAG, productName);
+            startActivity(goToOrderFromIntent);
+        });
+    }
+
+    private void setUpSettingsImageView() {
+
+        ImageView userSettingsImageView = findViewById(R.id.userProfileImageView2);
+
+        userSettingsImageView.setOnClickListener(V -> {
+            Intent goToUserSettingsIntent = new Intent(ProductListActivity.this, UserProfileActivity.class);
+            startActivity(goToUserSettingsIntent);
+        });
+    }
+
+    private void setUpProductListRecyclerView() {
+        RecyclerView productListRecyclerView = findViewById(R.id.productListRecyclerView);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        productListRecyclerView.setLayoutManager(layoutManager);
+
+        adapter = new ProductListRecyclerVIewAdapter(products, this);
+        productListRecyclerView.setAdapter(adapter);
+
+
+    }
+
+    private void setUpAddProductButton() {
+        Button addProductButton = findViewById(R.id.AddProductButton);
+        addProductButton.setOnClickListener(v -> {
+            Intent goToAddProductActivity = new Intent(ProductListActivity.this, AddProductActivity.class);
+            startActivity(goToAddProductActivity);
+        });
+    }
+
+    private void init() {
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        products = new ArrayList<>();
+    }
+}
