bd67e372ccfe3745625931a58573fca817e65667
ThaerHussein <tselawe706@gmail.com>
2023-11-21 17:07:10 +0300

    class 37

---
+++ class-37/Demo/BuyStuff/app/src/main/java/com/love2code/buystuff/activity/BuyStuffAmplifyApplication.java
@@ -0,0 +1,28 @@
+package com.love2code.buystuff.activity;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
+
+public class BuyStuffAmplifyApplication extends Application {
+
+    public static final String TAG= "buystuffapplication";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        try{
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSS3StoragePlugin());
+            Amplify.configure(getApplicationContext());
+        }catch (AmplifyException ae){
+            Log.e(TAG, "Error initializing Amplify" + ae.getMessage(), ae);
+        }
+    }
+}
